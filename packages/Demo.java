fc08b825ffee8992871a8d475ef4e6cf8ae9f96d
Maithri R <maithrir238@gmail.com>
2025-11-13 07:25:28 +0530

    Packages learning demo

---
+++ packages/Demo.java
@@ -0,0 +1,14 @@
+import packages.AdvCalc;
+import packages.Calc;
+
+public class Demo 
+{
+ public static void main(String[] args) 
+ {
+    Calc c = new Calc();
+    AdvCalc ac = new AdvCalc();
+    System.out.println(ac.div(19,2));
+    System.out.println(c.add(3,4));
+
+ }
+}
